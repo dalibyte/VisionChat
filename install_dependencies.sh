053df98688da3427febcf286bcc616323e197abb
Cap1tal1st <128931852+bunn1ez@users.noreply.github.com>
2023-09-22 17:00:06 -0400

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,15 @@
+### install_dependencies.sh
+```bash
+#!/bin/bash
+
+# Update package lists
+apt-get update
+
+# Install Tesseract
+apt-get install -y tesseract-ocr
+
+# Install Python libraries
+pip install opencv-python-headless pytesseract nltk openai
+
+# Download NLTK data
+python -c "import nltk; nltk.download('punkt')"
